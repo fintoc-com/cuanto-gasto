e1172d6b3d3d76ada9a6085d8e357f751c1adf47
Cristóbal  Griffero <cristobal.griffero@gmail.com>
2021-01-25 00:45:47 -0300

    feat(intro): improve view

--- app/javascript/views/intro.vue
+++ app/javascript/views/intro.vue
@@ -1,29 +1,52 @@
 <template>
   <div class="bg-gray-100 min-h-screen flex flex-col">
     <div class="container max-w-lg mx-auto flex-1 flex flex-col items-center justify-center px-2">
-      <img
-        class="mx-auto h-12 w-auto"
-        src="https://d651wjhrjhvon.cloudfront.net/fintoc-logo.svg"
-        alt="fintoc"
-      >
+      <h1 class="text-4xl">Cuánto<span class="font-bold">Gasto</span> 💸</h1>
       <h1 class="mt-5 text-3xl text-center font-regular text-gray-900">
-        Revisa cuánto gastas en delivery
+        Revisa cuánto gastas en delivery 😱
       </h1>
       <h1 class="m-5 text-xl text-center text-gray-800">
-        primero tienes que conectar tu cuenta de banco con <a href="https://fintoc.com">fintoc</a>
+        Primero tienes que conectar tu cuenta 🔌 de banco con <a href="https://fintoc.com">fintoc</a>
       </h1>
 
-      <button
-        type="submit"
-        v-on:click="openwidget"
-        class="group relative w-full flex justify-center py-4 px-4 border
-           border-transparent text-sm leading-5 rounded-md
-           text-white fintoc-blue focus:outline-none transition duration-150
-           ease-in-out">
-           conectar cuenta
+      <button 
+          @click="openwidget"
+          type="submit"
+          class="group relative w-full flex justify-center py-4 px-4 border
+                  border-transparent text-sm leading-5 font-medium rounded-md
+                  text-white fintoc-blue focus:outline-none transition duration-150
+                  ease-in-out focus:opacity-50"
+          :class="{ 'hover:opacity-50': !invalidForm, 'cursor-not-allowed': invalidForm }">
+        <span
+          class="absolute left-0 inset-y-0 flex items-center pl-3">
+          <svg class="h-5 w-5 text-white opacity-25 transition ease-in-out duration-150"
+              fill="currentColor"
+              viewBox="0 0 20 20">
+            <path fill-rule="evenodd"
+                  d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0
+                    01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                  clip-rule="evenodd" />
+          </svg>
+        </span>
+        Conectar cuenta
       </button>
-
-      <widget v-if="showwidget" v-bind:widgetWebhookHost="widgetWebhookHost" v-bind:session="session" @close="closewidget" @linkCreated="linkCreated"></widget>
+      <div class="mt-6">
+        <p class="font-bold text-xs">Powered by</p>
+        <a href="https://fintoc.com" target="_blank">
+          <img
+            class="mx-auto h-6 w-auto"
+            src="https://d651wjhrjhvon.cloudfront.net/fintoc-logo.svg"
+            alt="fintoc"
+          >
+        </a>
+      </div>
+      <a href="https://blog.fintoc.com/cuanto-gasto"
+         target="_blank"
+         class="mt-6 text-sm focus:outline-none hover:underline transition ease-in-out duration-150"
+      >
+        ¿Por qué hacemos esto?
+      </a>
+      <widget v-if="showWidget" v-bind:widgetWebhookHost="widgetWebhookHost" v-bind:session="session" @close="closewidget" @linkCreated="linkCreated"></widget>
     </div>
   </div>
 </template>
@@ -34,18 +57,18 @@ import widget from 'components/widget.vue';
 export default {
   data() {
     return {
-      showwidget: false,
+      showWidget: false,
     };
   },
   props: ['session', 'widgetWebhookHost'],
   components: { widget, },
   methods: {
      openwidget() {
-      this.showwidget = true;
+      this.showWidget = true;
      },
 
      closewidget() {
-      this.showwidget = false;
+      this.showWidget = false;
      },
 
     touchifpresentelsereset(field) {
@@ -65,7 +88,7 @@ export default {
 </script>
 
 <style scoped>
-  .vertical-slide-fade-enter,
+  /* .vertical-slide-fade-enter,
   .vertical-slide-fade-leave-to {
     opacity: 0;
     transform: translatey(-18px);
@@ -73,5 +96,5 @@ export default {
   .vertical-slide-fade-enter-active,
   .vertical-slide-fade-leave-active {
     transition: all .5s ease;
-  }
+  } */
 </style>
